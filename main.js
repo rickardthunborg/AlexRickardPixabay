345cbc2129b96e9f75088704ef6b7229dc133c61
alexxgru <alexgrundell@hotmail.com>
2023-02-13 11:15:47 +0100

    asd

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let form = document.querySelector('form');
 let imageList = document.querySelector('#images');
+let currentPage = 1;
 
 form.onsubmit = async event => {
 
@@ -8,12 +9,13 @@ form.onsubmit = async event => {
     let search = form.textsearch.value;
     let colorSelection = form.colorselect.value;
 
-    let url = `https://pixabay.com/api/?key=33602531-81aa9b6b285c0a9184743cc53&q=${search}&per_page=10`
+    let url = `https://pixabay.com/api/?key=33602531-81aa9b6b285c0a9184743cc53` +
+        `&q=${search}&per_page=10&page=${currentPage}`
 
     let response = await fetch(url);
     let json = await response.json();
 
-    for (i = 0; i < 10; i++){
+    for (i = 0; i < json.hits.length; i++) {
         let li = document.createElement('li')
         let img = document.createElement('img');
         let h2 = document.createElement('h2')
@@ -24,13 +26,15 @@ form.onsubmit = async event => {
         img.src = imgLink;
         h2.textContent = json.hits[i].tags;
         p.textContent = "taken by: " + json.hits[i].user;
-    
+
         li.append(img);
         li.append(h2)
         li.append(p)
         imageList.append(li);
     }
+}
 
-
+function toggleButtons(json) {
 
 }
+
